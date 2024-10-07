26e077855e20fc1bc8cbaf738db3d2503ef7cb2c
bao <tienbaoams@gmail.com>
2024-10-07 16:27:05 +1100

    feature/userprofile

--- app/models/user_account.py
+++ app/models/user_account.py
@@ -1,6 +1,12 @@
+from datetime import date
+from typing import Optional
 from pymongo import ASCENDING, IndexModel
 
-from app.models.base import RootModel
+from app.models.base import RootModel, RootEnum
+
+class Gender(str, RootEnum):
+    MALE = "MALE"
+    FEMALE = "FEMALE"
 
 class UserAccount(RootModel):
     class Collection:
@@ -14,7 +20,14 @@ class UserAccount(RootModel):
             )
         ]
 
-    user_name: str
+    name: str
     email: str
     #hash password before put in db / best do it from FE
     password: str
+    weight: Optional[float] # kg
+    height: Optional[float] # cm
+    date_of_birth: date #YYYY-MM-DD
+    gender: Optional[Gender] # MALE, FEMALE or OTHER(null)
+    allergies: list[str]
+    dietary_preferences: list[str]
+    profile_picture: Optional[str] # Profile id
